69605a2a574dc11e672c01120f4ef35ba9017513
tkqengineer <tkqengineer@163.com>
2017-06-13 14:30:45 +0800

    duubo

---
+++ dubbo/interface/src/main/java/com/alex/service/HelloService.java
@@ -0,0 +1,8 @@
+package com.alex.service;
+
+
+public interface HelloService {
+
+    String hello();
+
+}
---
+++ dubbo/interface/src/main/java/com/alex/service/LK.java
@@ -0,0 +1,14 @@
+package com.alex.service;
+
+/**
+ * @classDesc: 功能描述:()
+ * @author: 滕康全
+ * @createTime: 2017/6/12 16:29:44
+ * @version: v1.0
+ * @copyright:善林(上海)金融信息服务有限公司
+ */
+public class LK {
+
+
+
+}
